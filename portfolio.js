e7f378d5b8a5db9bb29b3b6dfba978bee6e74636
ChristopherSaury <saurychristopher@gmail.com>
2021-08-05 09:58:23 +0200

    typed jquery plugin has been added

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Développeur web junior", "Soyez les bienvenus sur mon portfolio"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
